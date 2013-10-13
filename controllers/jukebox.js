7daa5a8ab834fcdde337434e98375827790f9bab
Alex Scotton <alex@alexscotton.com>
2013-10-13 13:48:33 +0100

    Trying to get playUri working, pushed to pull

--- controllers/jukebox.js
+++ controllers/jukebox.js
@@ -19,6 +19,13 @@ function jukebox(options) {
 		webSocketUrl: "ws://localhost:6680/mopidy/ws/"
 	});
 
+	this.status = {
+
+		'now_playing' : null,
+
+	};
+
+	this.queue = [];
 
 	events.EventEmitter.call(this);
 
@@ -26,41 +33,68 @@ function jukebox(options) {
 
 		console.log('Mopidy Online');
 
-		this.mopidy.playback.getCurrentTrack().then(jukebox.printTrack, console.error.bind(console));
+		// this.library.playUri('spotify:track:0bM5JsBjWU4RyYlMbp0voY').bind(this);
 
-	},
+
+	};
+
 	this.printTrack = function (track) {
 		
 		if (track) {
+
 			console.log("Currently playing:", track.name, "by",	track.artists[0].name, "from", track.album.name);
+
 		} else {
+
 			console.log("No current track");
+
 		}
 		
-	},
-	this.playbackStarted = function (track) {
-		// console.log(track);
-		this.printTrack(track.tl_track.track);
+	};
 
-		this.emit('playback:started', {
+	this.playbackStarted = function (track) {
+		console.log('playback started');
+		var pretty_track = {
+
 			'name'	: track.tl_track.track.name,
 			'artist': track.tl_track.track.artists[0].name,
 			'album'	: track.tl_track.track.album.name,
-		});
+			'uri'	: track.tl_track.track.uri,
 		
-	},
+		};
+
+		console.log(pretty_track);
+
+		this.status.now_playing = pretty_track;
+
+		this.emit('playback:started', pretty_track);
+
+	};
+
 	this.nextTrack = function () {
-		this.mopidy.playback.next();
-	},
+
+		this.mopidy.playback.next().then(null, console.error.bind(console));
+
+	};
+
 	this.previousTrack = function() {
-		this.mopidy.playback.previous();
-	},
+
+		this.mopidy.playback.previous().then(null, console.error.bind(console));
+
+	};
+
 	this.pause = function() {
-		this.mopidy.playback.pause();
-	},
-	this.play = function() {
-		this.mopidy.playback.play();
-	},
+
+		this.mopidy.playback.pause().then(null, console.error.bind(console));
+
+	};
+
+	this.play = function(track) {
+
+		this.mopidy.playback.play(track).then(console.error.bind(console), console.error.bind(console));
+
+	};
+
 	this.library = {
 
 		/*
@@ -69,15 +103,6 @@ function jukebox(options) {
 		 # Returns results matching 'a' in any backend
 		 * search({'any': ['a']})
 		 * search(any=['a'])
-
-		 # Returns results matching artist 'xyz' in any backend
-		 * search({'artist': ['xyz']})
-		 * search(artist=['xyz'])
-
-		 # Returns results matching 'a' and 'b' and artist 'xyz' in any
-		 # backend
-		 * search({'any': ['a', 'b'], 'artist': ['xyz']})
-		 * search(any=['a', 'b'], artist=['xyz'])
 
 		 # Returns results matching 'a' if within the given URI roots
 		 # "file:///media/music" and "spotify:"
@@ -89,18 +114,41 @@ function jukebox(options) {
 			console.log('search called');
 
 			mopidy.library.search(params).then(
+
 				function(data) {
+
 					console.log('search complete');
+
 					callback(null, data);
+
 				},
-				console.error
+
+				console.error.bind(console)
+
 			);
 
+		},
+
+		playUri : function(uri) {
+
+			mopidy.library.lookup(uri).then(function(track) {
+			
+				console.log('playUri');
+
+				this.mopidy.tracklist.add([track])
+			
+				this.play();
+			
+			}.bind(this), console.error.bind(console));
+			
 		}
 	}
 
 	this.mopidy.on('state:online', this.online.bind(this));
 	this.mopidy.on('event:trackPlaybackStarted', this.playbackStarted.bind(this));
+
+	// Dev - echo all events
+	// this.mopidy.on(console.log.bind(console));
 
 }
 
