d1b642608433e005ded6847a5be1e94e99508d4c
Shaun Church <shaun@shaunchurch.com>
2014-05-27 21:09:09 +0100

    update less middleware call, add socket.io to package.json

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(require('less-middleware')(__dirname + '/public' ));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
